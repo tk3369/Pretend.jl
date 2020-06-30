0640823cf64a28d715075becc9104468610b11e8
Tom Kwong <tk3369@gmail.com>
2020-06-30 00:21:30 -0700

    ✅ Added test from Mocking.jl README

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ Pretend.activate()
 
     # duck typing
     @mockable add(x, y) = x + y
-    
+
     # applying a patch to the add function
     apply(add => (x, y) -> 0) do
         @test add(1, 2) == 0
@@ -30,6 +30,22 @@ Pretend.activate()
         @test add(3.0, 3.0) == 27.0   # Float64 patch
     end
 
+    # Mocking.jl example
+    @mockable open(f,io) = Base.open(f,io)
+    @mockable read(fp, n) = Base.read(fp, n)
+
+    function randdev(n::Integer)
+        open("/dev/urandom") do fp
+            reverse(read(fp, n))
+        end
+    end
+
+    # patch `read` to test properly
+    # patch `open` to avoid opening any file
+    @test apply(read => (fp,n) -> [UInt8(i) for i in 1:n],
+                open => (f,io) -> f(io)) do
+        randdev(5)
+    end == [0x05, 0x04, 0x03, 0x02, 0x01]
 
     # Design changes
     # - ???
@@ -42,7 +58,7 @@ Pretend.activate()
     # Not supported:
     # - thread safety (cannot run mocked processes concurrently)
 
-    #= 
+    #=
     Syntax: @when macro to apply patch only for specific value or type of arguments
     @when add [x = 1] => 0
     @when add [x isa Int, y isa Float64] => 0
